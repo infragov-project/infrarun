d813b741d5e026f3e0dcf1030c1528de425dbdf3
Carlos Felgueiras <catofel2000@gmail.com>
2025-08-15 18:51:08 +0100

    refactor: split tool definition (schema and parsing) and embed tools code in two files within the same package

--- internal/core/tools/definition.go
+++ internal/core/tools/definition.go
@@ -1,9 +1,7 @@
 package tools
 
 import (
-	"embed"
 	"fmt"
-	"io/fs"
 	"regexp"
 
 	"gopkg.in/yaml.v3"
@@ -139,43 +137,4 @@ func (w *outputWrapper) UnmarshalYAML(value *yaml.Node) error {
 	}
 
 	return nil
-}
-
-//go:embed definitions/*.yaml
-var embedTools embed.FS
-
-func GetEmbedToolDefinitions() map[string]Tool {
-	tools := make(map[string]Tool)
-
-	fileNames, err := fs.Glob(embedTools, "definitions/*.yaml")
-
-	if err != nil {
-		return tools
-	}
-
-	for _, fileName := range fileNames {
-		yamlContent, err := embedTools.ReadFile(fileName)
-
-		if err != nil {
-			continue
-		}
-
-		var definition toolDefinition
-
-		err = yaml.Unmarshal(yamlContent, &definition)
-
-		if err != nil {
-			continue
-		}
-
-		tool, err := toolFromDefinition(definition)
-
-		if err != nil {
-			continue
-		}
-
-		tools[tool.Name] = *tool
-	}
-
-	return tools
 }
