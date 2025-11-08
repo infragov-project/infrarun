5b7f2af7c03b45713b0f25e17f7a6555687260fd
Carlos Felgueiras <catofel2000@gmail.com>
2025-11-08 23:56:07 +0000

    fix(internal/parsers): added a custom parser for kics that inserts warning as the level of all results, since by default it fills them with empty strings (#17)

--- internal/core/tools/parser.go
+++ internal/core/tools/parser.go
@@ -12,6 +12,7 @@ type ResultParser func([]byte) (*sarif.Report, error)
 var parsers = map[string]ResultParser{
 	"sarif":  parseJsonSARIF,
 	"glitch": glitch.ParseGlitch,
+	"kics":   kicsParser,
 }
 
 func GetParser(name string) (ResultParser, error) {
@@ -29,6 +30,25 @@ func parseJsonSARIF(data []byte) (*sarif.Report, error) {
 
 	if err != nil {
 		return nil, err
+	}
+
+	return parsed, nil
+}
+
+// Temporary parser for KICS, since it doesn't fill the level of each result
+// and instead puts and empty string. Some SARIF consumers consider that
+// invalid SARIF.
+func kicsParser(data []byte) (*sarif.Report, error) {
+	parsed, err := parseJsonSARIF(data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, run := range parsed.Runs {
+		for _, res := range run.Results {
+			res.Level = "warning"
+		}
 	}
 
 	return parsed, nil
